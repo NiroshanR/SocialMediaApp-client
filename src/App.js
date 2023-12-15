a002515339b20b98415a5fa3204a33d5a1789fd5
NiroshanR <niroshanragu@gmail.com>
2023-12-15 18:32:29 +0530

    client side code added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,62 @@
-import logo from './logo.svg';
+import {useEffect, useState} from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import NavbarComponent from './components/NavbarComponent'
+import { Container, Row, Col, Card, Button} from 'react-bootstrap'
+import axios from 'axios'
+
 
 function App() {
+  const [users, setUsers] = useState([])
+
+
+  useEffect(() =>{
+    fetchUser()
+  }, [])
+
+  // This data should be fetched from a form
+  const dummyUser ={
+    first_name: 'lakshman',
+    last_name: 'Raj',
+    email: 'lakshman@gmail.com',
+    avatar: 'https://reqres.in/img/faces/9-image.jpg'
+  }
+
+  const fetchUser = () => {
+    axios.get('http://localhost:4000/users')   
+    .then((response) => setUsers(response.data.data))
+    .catch((error) => console.log(error.message))
+  }
+
+  const createNewUser = async() =>{
+    axios.post('http://localhost:4000/users', dummyUser)   
+    .then((response) => fetchUser())
+    .catch((error) => console.log(error.message))
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <NavbarComponent/>
+      <Container>
+        <Row>
+          {users.map((user) => (
+               <Col md={4} Key={user.id}>
+                   <Card style={{ width: '18rem', margin: '10px auto'}}>
+                      <Card.Img variant="top" src={user.avatar} />   
+                        <Card.Body>
+                            <Card.Title>{user.first_name} {user.last_name}</Card.Title>
+                            <Card.Text>
+                              {user.email}
+                            </Card.Text>
+                            <Button variant="dark">Connect</Button>
+                        </Card.Body>
+                  </Card>       
+              </Col>                
+          ))}
+        
+        </Row>
+        <Button variant='dark' onClick={createNewUser}>Add new user</Button>
+      </Container>
     </div>
   );
 }
---
+++ src/components/NavbarComponent.js
@@ -0,0 +1,39 @@
+import Button from 'react-bootstrap/Button';
+import Container from 'react-bootstrap/Container';
+import Form from 'react-bootstrap/Form';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+
+
+function NavbarComponent() {
+  return (
+    <Navbar expand="lg" className="bg-body-tertiary"  bg="dark" data-bs-theme="dark">
+      <Container fluid>
+        <Navbar.Brand href="#">Social Media App</Navbar.Brand>
+        <Navbar.Toggle aria-controls="navbarScroll" />
+        <Navbar.Collapse id="navbarScroll">
+          <Nav
+            className="me-auto my-2 my-lg-0"
+            style={{ maxHeight: '100px' }}
+            navbarScroll
+          >
+            <Nav.Link href="#">Home</Nav.Link>
+            <Nav.Link href="#">About us</Nav.Link>
+            <Nav.Link href="#"> Contact Us</Nav.Link>
+          </Nav>
+          <Form className="d-flex">
+            <Form.Control
+              type="search"
+              placeholder="Search"
+              className="me-2"
+              aria-label="Search"
+            />
+            <Button variant="outline-success">Search</Button>
+          </Form>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+}
+
+export default NavbarComponent;
